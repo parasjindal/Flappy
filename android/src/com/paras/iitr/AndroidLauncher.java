3438e23c7697e870b313d47dc6d9007373c5a54f
Paras Jindal <parasjindal96@gmail.com>
2016-08-31 15:19:00 +0530

    first commit

---
+++ android/src/com/paras/iitr/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.paras.iitr;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.paras.iitr.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ core/src/com/paras/iitr/States/State.java
@@ -0,0 +1,26 @@
+package com.paras.iitr.States;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by Paras on 31-08-2016.
+ */
+public abstract class State {
+//    protected OrthographicCamera cam;
+//    protected Vector3 mouse;
+//    protected GameStateManager gsm;
+//
+//    protected State(GameStateManager gsm){
+//        this.gsm=gsm;
+//        cam=new OrthographicCamera();
+//        mouse=new Vector3();
+//
+//    }
+//
+//    protected abstract handleInput();
+//    protected abstract update();
+//    protected abstract render(SpriteBatch sb);
+
+}
